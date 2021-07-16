f56fa05f50071a2df0717a84f19edf43f0b79dee
amalia <amaliarasyid1999@gmail.com>
2021-07-16 17:39:32 +0700

    change text color on tab and the tab title

--- app/src/main/java/com/kontrakanprojects/appbekamcbr/view/info/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/kontrakanprojects/appbekamcbr/view/info/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.kontrakanprojects.appbekamcbr.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_disease,
+    R.string.tab_solution
 )
 
 /**
